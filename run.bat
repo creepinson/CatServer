0648bee1fc120d8e24d1db37a1888a2069e97050
CatServer <47019948+CatServer@users.noreply.github.com>
2019-01-27 15:03:56 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -XX:+UserG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-Personal-289f343-universal.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-Personal-289f343-universal.jar
 pause
