e0d027d79fffd363cecbfeb0b9889f4a70f47bb0
CatServer <luenlin18686@gmail.com>
2019-02-10 19:46:17 +0800

    update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-Community-06a5e15-universal.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-Community-d1ef9fd-universal.jar
 pause
