38fd9286c9bee89ebea19a9db687fbefd36d9b3b
CatServer <luenlin18686@gmail.com>
2019-03-12 18:14:33 +0800

    Fix issue #9 WorldDate save Fix issue #10 AppleCore

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-97e64db-Community.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-1468a63-Community.jar
 pause
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-97e64db-Community.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-1468a63-Community.jar
