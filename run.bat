792d829987bddbeaf6033e04d959fdd3d9bf7cae
IceSoulHanxi <icesoulhanxi@outlook.com>
2019-05-19 22:48:35 +0800

    Disable compatible Skript, it cause some bug

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-4894c2a-Community.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-4749966-Community.jar
 pause
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-4894c2a-Community.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-4749966-Community.jar
