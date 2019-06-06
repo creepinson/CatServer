d787ad8bd26040bdbf7d5bcb315fd0cfe7bc485a
IceSoulHanxi <icesoulhanxi@qq.com>
2019-06-06 21:07:40 +0800

    Disable DataSerializer

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-ce5a5e8-Community.jar
+java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-4b6a920-Community.jar
