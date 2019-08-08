9197fea251abde10735e906ac43b4e998fa190a2
恒林碳云 <47019948+CatServer@users.noreply.github.com>
2019-08-08 13:49:15 +0800

    Update to 3.0

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-917005d-Community.jar
-pause
--- run.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-java -XX:+UseG1GC -XX:+AggressiveOpts -XX:+UseCompressedOops -Xmx4096M -Xms1024M -jar CatServer-git-917005d-Community.jar
